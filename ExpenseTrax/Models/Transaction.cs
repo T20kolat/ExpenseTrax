0f80ab1e8b2599a5b451c9a605977e04e27cb664
T20kolat <laasasenaholassi@gmail.com>
2024-04-16 11:23:58 +0300

    Base for SQL DBs created

---
+++ ExpenseTrax/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTrax.Models
+{
+    public class Transaction
+    {
+        [Key] 
+        public int TransactionId { get; set; }
+        
+        //CatergoryId
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
--- ExpenseTrax/Program.cs
+++ ExpenseTrax/Program.cs
@@ -1,3 +1,6 @@
+using ExpenseTrax.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace ExpenseTrax
 {
     public class Program
@@ -8,6 +11,9 @@ namespace ExpenseTrax
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
             var app = builder.Build();
 
